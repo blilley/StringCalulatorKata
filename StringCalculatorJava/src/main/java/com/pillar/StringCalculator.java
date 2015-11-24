bf65e41efbb5c15ac4f062f0085d9737b3007a85
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-24 08:00:53 -0500

    Passing Null test

--- StringCalculatorJava/src/main/java/com/pillar/StringCalculator.java
+++ StringCalculatorJava/src/main/java/com/pillar/StringCalculator.java
@@ -2,8 +2,12 @@ package com.pillar;
 
 public class StringCalculator {
     public static Integer add(String numbers) {
+        if(numbers == null)
+            throw new RuntimeException("Invalid Input");
+
         if(numbers.isEmpty())
             return 0;
+        
         Integer sum = Integer.parseInt(numbers);
         return sum;
     }
