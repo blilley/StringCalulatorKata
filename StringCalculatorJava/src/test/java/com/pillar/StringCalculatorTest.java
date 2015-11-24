08b560b2f2b7c8a6e806c2821453c89d0e42ed5f
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-24 08:00:03 -0500

    Missing null test

--- StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
+++ StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
@@ -1,11 +1,24 @@
 package com.pillar;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StringCalculatorTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void add_nullString_ThrowsException() {
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Invalid Input");
+
+        StringCalculator.add(null);
+    }
+
     @Test
     public void add_emptyString_Returns0(){
         Integer result = StringCalculator.add("");
