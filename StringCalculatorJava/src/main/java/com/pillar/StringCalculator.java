eca1f20ca660781103f6d3ba6ad2208a6a13939f
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-24 08:01:58 -0500

    Refactoring

--- StringCalculatorJava/src/main/java/com/pillar/StringCalculator.java
+++ StringCalculatorJava/src/main/java/com/pillar/StringCalculator.java
@@ -2,13 +2,19 @@ package com.pillar;
 
 public class StringCalculator {
     public static Integer add(String numbers) {
-        if(numbers == null)
-            throw new RuntimeException("Invalid Input");
+        Guard.notNull(numbers);
 
         if(numbers.isEmpty())
             return 0;
-        
+
         Integer sum = Integer.parseInt(numbers);
         return sum;
+    }
+
+    private static class Guard {
+        public static void notNull(String numbers) {
+            if(numbers == null)
+                throw new RuntimeException("Invalid Input");
+        }
     }
 }
