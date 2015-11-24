a28bdce81af417cc53eb9e6de883dc612453f188
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-24 07:53:07 -0500

    Still failing first test

--- StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
+++ StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
@@ -2,8 +2,6 @@ package com.pillar;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-
 public class StringCalculatorTest {
     @Test
     public void add_emptyString_Returns0(){
