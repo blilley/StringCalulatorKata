dca4a149b96baa72214ce7207775e1488edcb4d0
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-24 07:46:06 -0500

    Java setup

---
+++ StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.pillar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StringCalculatorTest {
+    @Test
+    public void nothing(){
+        assertTrue(true);
+    }
+}
