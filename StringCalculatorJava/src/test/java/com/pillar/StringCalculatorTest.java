12a3756810a570f66e8a31e154defdfc9fbacb1f
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-24 07:54:22 -0500

    First Failing test

--- StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
+++ StringCalculatorJava/src/test/java/com/pillar/StringCalculatorTest.java
@@ -2,9 +2,13 @@ package com.pillar;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class StringCalculatorTest {
     @Test
     public void add_emptyString_Returns0(){
         Integer result = StringCalculator.add("");
+        assertThat(result, is(0));
     }
 }
